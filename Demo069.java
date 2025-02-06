4214587ed30275c0e071f64038431f0bd2fe8a66
SathwikPatel !! <Sathwikpatel6777@gmail.com>
2025-02-06 12:08:08 +0530

    Index values of an arrays

---
+++ Demo069.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class SumOfArrayElements {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements in the array: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum += array[i];
+        }
+
+        System.out.println("Sum of all the elements in the array: " + sum);
+
+        scanner.close();
+    }
+}
